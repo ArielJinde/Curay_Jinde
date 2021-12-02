156205c4fe2b9c9638be4cd36a24a209168371d2
Ariel <ajinde3033@uta.edu.ec>
2021-12-01 19:47:40 -0500

    19:45  commit

--- app/src/main/java/example/curay_jinde/MainActivity.java
+++ app/src/main/java/example/curay_jinde/MainActivity.java
@@ -1,13 +1,25 @@
 package example.curay_jinde;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button  btn_siguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_siguiente = findViewById(R.id.button_siguiente);
+
+
+    }
+
+    public  void  onClickSiguente(View view)
+    {
+        Intent  intent = new Intent(this,SegundoActivity.class);
+        this.startActivity(intent);
     }
 }
--- app/src/main/java/example/curay_jinde/SegundoActivity.java
+++ app/src/main/java/example/curay_jinde/SegundoActivity.java
@@ -1,13 +1,25 @@
 package example.curay_jinde;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SegundoActivity extends AppCompatActivity {
-
+    Button btn_siguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
+        btn_siguiente = findViewById(R.id.button_siguiente);
+
+
+    }
+
+    public  void  onClickSiguente(View view)
+    {
+        Intent intent = new Intent(this,TercerActivity.class);
+        this.startActivity(intent);
     }
 }
